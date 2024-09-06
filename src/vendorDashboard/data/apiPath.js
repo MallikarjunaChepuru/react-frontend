31bd5cfdcf0b31eea6ffbbfb37ac4328fe4f7d97
MallikarjunaChepurue <mallich178@gmail.com>
2024-09-06 16:32:38 +0530

    apiUrl chnaged

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,3 @@
 //export const API_URL="http://localhost:6060"
-export const API_URL="https://backend-nodejs-suby-bf6w.onrender.com/"
+export const API_URL="https://backend-nodejs-suby-bf6w.onrender.com"
+
