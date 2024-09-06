731f270e883a3350407d88969865035af4d01ad4
MallikarjunaChepurue <mallich178@gmail.com>
2024-09-06 16:22:47 +0530

    changed path to live i apiUrl

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,2 @@
-export const API_URL="http://localhost:6060"
-
-//export const API_URL="https://backend-nodejs-suby-bf6w.onrender.com/"
+//export const API_URL="http://localhost:6060"
+export const API_URL="https://backend-nodejs-suby-bf6w.onrender.com/"
